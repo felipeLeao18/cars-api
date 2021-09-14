029c871cebc75fbaa448fefdfa501558cc5b2f67
felipe leao <flpleao1807@gmail.com>
2021-09-14 19:18:00 -0300

    feat: Get erros with instanceOf Error

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
-import express from 'express'
+import 'reflect-metadata'
+import express, {Request,Response, NextFunction} from 'express'
 import mongoose from 'mongoose'
+import 'express-async-errors'
+
 import {connectionDb} from './Config/Database'
 import carRouter from './Routes/CarRoutes'
+import userRouter from './Routes/UserRoutes'
+
+
 const app = express()
-
-
 mongoose.connect(connectionDb)
+
+
 app.use(express.json())
 app.use('/cars',carRouter)
+app.use('/user',userRouter)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return res.status(400).json({
+        error: err.message
+      })
+    }
+  
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error'
+    })
+  })
+  
 
 app.listen(3000, () => {
     console.log("Server is on")
